3126d602ae443d08eedcdc3dc3c173761d9ea38a
Ricardo Felix Monteiro Neto <ricardo.felix@ciandt.com>
2023-09-30 16:09:27 -0300

    Creating first model class

--- djangoCourse/clientes/models.py
+++ djangoCourse/clientes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=70, blank=False, null=False)
+    endereco = models.CharField(max_length=200, blank=False, null=False)
