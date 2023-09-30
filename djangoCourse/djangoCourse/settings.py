62729dfae93179bce50d7d4c9c861c80cdd2bfd9
Ricardo Felix Monteiro Neto <ricardo.felix@ciandt.com>
2023-09-30 15:00:13 -0300

    modifying SQlite database name

--- djangoCourse/djangoCourse/settings.py
+++ djangoCourse/djangoCourse/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'djangoCourse.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'django_course.sqlite3',
     }
 }
 
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
