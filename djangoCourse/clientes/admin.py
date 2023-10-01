1380fbe18d0e30cd05979912f6f779b463ee6894
Ricardo Felix Monteiro Neto <ricardo.felix@ciandt.com>
2023-09-30 22:07:54 -0300

    Cadastrando novos models no Django admin

--- djangoCourse/clientes/admin.py
+++ djangoCourse/clientes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import (Cliente, 
+                     Telefone, 
+                     CPF,
+                     Departamento)
+
 
 # Register your models here.
+
+# Essa linha registra o model no Admin do Django, podemos acessar via navegador em http://127.0.0.1:8000/admin
+admin.site.register(Cliente)
+admin.site.register(Telefone)
+admin.site.register(CPF)
+admin.site.register(Departamento)
